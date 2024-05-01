dfd0d235099be7e41c41cf2f061acd92bfad06f1
Tiago Rodrigues <dev@tnrodrigues.pt>
2024-05-01 22:39:33 +0100

    Refactor verifyOtp route to use async/await and handle errors

--- app/api/verifyOtp/route.ts
+++ app/api/verifyOtp/route.ts
@@ -1,22 +1,31 @@
-import { NextRequest, NextResponse } from 'next/server';
-import twilio from 'twilio';
+import { NextRequest, NextResponse } from 'next/server'
+import twilio from 'twilio'
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-  const { phoneNumber, otp } = await req.json();
-  const verifySid = process.env.TWILIO_VERIFY_SID;
+  try {
+    const client = twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
+    )
+    const { phoneNumber, otp } = await req.json()
+    console.log(phoneNumber, otp)
+    const verifySid = process.env.TWILIO_VERIFY_SID
 
-  if (!verifySid) {
-    throw new Error('TWILIO_VERIFY_SID is not defined');
-  }
+    if (!verifySid) {
+      throw new Error('TWILIO_VERIFY_SID is not defined')
+    }
 
-  const verificationCheck = await client.verify.v2.services(verifySid)
-    .verificationChecks
-    .create({ to: phoneNumber, code: otp });
+    const verificationCheck = await client.verify.v2
+      .services(verifySid)
+      .verificationChecks.create({ to: phoneNumber, code: otp })
 
-  if (verificationCheck.status === 'approved') {
-    return NextResponse.json({ message: "sucess", status: 200 });
-  } else {
-    return NextResponse.json({ message: "ardeu", status: 400 });
+    if (verificationCheck.status === 'approved') {
+      return NextResponse.json({ message: 'sucess', status: 200 })
+    } else {
+      return NextResponse.json({ message: 'ardeu', status: 400 })
+    }
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({ message: 'Internal Server Error', status: 500 })
   }
 }
