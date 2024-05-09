1e0861fce24aa1b8ae3f47ab50457aeed50777c6
Rafael Coias <rafaelmsppcoias@gmail.com>
2024-05-10 00:49:48 +0100

    add brandfeels icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <link
+        rel="icon"
+        href="/content/logos/icon.png"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   )
